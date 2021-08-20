a448371ebbd1659ea2a12d366d78a572ed300db9
VitArts <vitarts@ya.ru>
2021-08-20 13:31:07 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let newN = '';
+	n = String(n)
+	for(let i = 0; i < n.length; i++) {
+   newN = n[i] + newN
+	}
+
+	if (newN[newN.length - 1] === '-') {
+		newN = newN.slice(0, -1)
+	}
+
+	return newN
 }
